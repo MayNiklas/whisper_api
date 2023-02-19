b8d287b107f4362355c8691be0059f45b28c290f
MayNiklas <info@niklas-steffen.de>
2023-02-19 10:47:30 +0100

    add stuff

--- whisper_api/main.py
+++ whisper_api/main.py
@@ -1,13 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+DEFAULT_MODEL = "large-v2"
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/v1/transcribe")
+async def transcribe(file: bytes = File()):
+    """
+    Transcribe audio file.
+    :param file: Uploaded audio file.
+    :return: Transcription of audio file.
+    """
+
+    # test via:
+    # curl --location --request POST 127.0.0.1:8081/v1/transcribe -F "file=@audio.m4a"
+    return {"file_size": len(file)}
