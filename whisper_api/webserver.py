29594e57a33f0fffff8affbefe09b9331c5c193c
MayNiklas <info@niklas-steffen.de>
2023-02-19 10:52:45 +0100

    add doc

--- whisper_api/webserver.py
+++ whisper_api/webserver.py
@@ -7,7 +7,11 @@ def start():
     """
     Start the webserver.
     Run the app with:
+
     uvicorn main:app --reload --host 127.0.0.1 --port 8081
+    uvicorn whisper_api:app --reload --host 127.0.0.1 --port 8081
+
+    whisper_api
     """
 
     uvicorn.run(app, host="127.0.0.1", port=8081)
