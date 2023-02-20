38344bc99ef4d14f9cbf0276a2ba426d69a66169
MayNiklas <info@niklas-steffen.de>
2023-02-20 18:10:09 +0100

    aua

--- whisper_api/main.py
+++ whisper_api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, UploadFile
 
 from .models import Task
+from . import convert
 
 app = FastAPI()
 
@@ -19,11 +20,41 @@ async def transcribe(file: UploadFile):
     """
 
     # Create a new task
-    # Task contains a temporary file to store the audio file
-    # and a unique identifier
     task = Task()
     task.audiofile.write(await file.read())
 
+    # Append task to pool of tasks
     tasks.append(task)
 
+    # return task id
     return {"task_id": task.uuid}
+
+
+@app.get("/v1/status/{task_id}")
+async def status(task_id: str):
+    """
+    Get the status of a task.
+    :param task_id: ID of the task.
+    :return: Status of the task.
+    """
+    for task in tasks:
+        if str(task.uuid) == task_id:
+            return {"task_id": task.uuid, "status": task.status, "result": task.result}
+
+
+@app.get("/v1/work")
+async def work():
+    """
+    Get a task to work on.
+    :return: Task to work on.
+    """
+    for task in tasks:
+        if task.status == "pending":
+            task.status = "working"
+
+            result = convert.transcribe(task.audiofile.name)
+
+    return {
+        "language": result["language"],
+        "transcript": result["text"],
+    }
