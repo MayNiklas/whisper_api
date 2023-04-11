12f8383211d45256af687df2173119492dae59aa
MayNiklas <info@niklas-steffen.de>
2023-04-11 10:47:04 +0200

    nixpkgs = nixos-unstable-small

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/master";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
