edd34726f24c8a23936092f09b27ba66b3ffeb53
nonchris <git@chris-ge.de>
2023-04-29 21:20:38 +0200

    add startup task starting process and listening to pipe

--- src/whisper_api/__init__.py
+++ src/whisper_api/__init__.py
@@ -17,9 +17,11 @@ if __package__ is None and not hasattr(sys, "frozen"):
     sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
 
 
-from whisper_api.environment import API_PORT, API_LISTEN
+from whisper_api.environment import API_PORT, API_LISTEN, KEEP_MODEL_IN_MEMORY
 from whisper_api.version import __version__
 from whisper_api.api_endpoints import endpoints
+
+import whisper_api.decoding.decoder as decoder
 
 description = """
 Whisper API transcribes audio files.
@@ -56,6 +58,27 @@ app.add_middleware(
 conn_to_parent, conn_to_child = multiprocessing.Pipe(duplex=True)
 
 api_end_points = EndPoints(app, task_dict, open_audio_files_dict, conn_to_child)
+
+
+@app.on_event("startup")
+def listen_to_child():
+    """ Start decode-process, listen to it and update the task_dict accordingly """
+
+    decoder_process = multiprocessing.Process(target=decoder.Decoder.init_and_run,
+                                              args=(conn_to_child, conn_to_parent, KEEP_MODEL_IN_MEMORY)
+                                              )
+    decoder_process.start()
+
+    while True:
+        task_update_json = conn_to_parent.recv()
+        task = Task.from_json(task_update_json)
+
+        task_dict[task.uuid] = task
+
+        # when task is done (no matter if finished or failed) close and delete the audio file
+        if task.status == "finished" or task.status == "failed":
+            open_audio_files_dict[task.audiofile_name].close()
+            del open_audio_files_dict[task.audiofile_name]
 
 
 def start():
