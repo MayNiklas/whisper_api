1460d8e0ebf43a19cfc6a0233c61e6af450df0ff
nonchris <git@chris-ge.de>
2023-04-29 21:14:59 +0200

    add env varible KEEP_MODEL_IN_MEMORY

--- src/whisper_api/environment.py
+++ src/whisper_api/environment.py
@@ -2,3 +2,4 @@ import os
 
 API_PORT = int(os.getenv("PORT", 3001))
 API_LISTEN = os.getenv("LISTEN", "127.0.0.1")
+KEEP_MODEL_IN_MEMORY = os.getenv("KEEP_MODEL_IN_MEMORY", "True").lower() == "true"
