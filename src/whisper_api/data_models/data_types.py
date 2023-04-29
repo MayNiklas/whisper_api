035189bd7ea4a88f9e81c051ceefe3282df77638
nonchris <git@chris-ge.de>
2023-04-29 20:36:30 +0200

    add fourth state 'failed' to task status

--- src/whisper_api/data_models/data_types.py
+++ src/whisper_api/data_models/data_types.py
@@ -2,6 +2,6 @@ from typing import Literal
 
 uuid_hex_t = str
 task_type_str_t = Literal["transcribe", "translate"]
-status_str_t = Literal["pending", "processing", "finished"]
+status_str_t = Literal["pending", "processing", "finished", "failed"]
 model_sizes_str_t = Literal["base", "small", "medium", "large"]
 named_temp_file_name_t = str
