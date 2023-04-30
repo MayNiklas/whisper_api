9f1faed88ee77a29224b7265d025081efe56e1f7
nonchris <git@chris-ge.de>
2023-04-30 02:35:53 +0200

    change way DEVELOP_MODE is parsed from env

--- src/whisper_api/environment.py
+++ src/whisper_api/environment.py
@@ -2,8 +2,8 @@ import os
 
 API_PORT = int(os.getenv("PORT", 3001))
 API_LISTEN = os.getenv("LISTEN", "127.0.0.1")
-DEVELOP_MODE = os.getenv("DEVELOP_MODE", "False").lower() == "true"
 LOAD_MODEL_ON_STARTUP = int(os.getenv("LOAD_MODEL_ON_STARTUP", 1))
+DEVELOP_MODE = int(os.getenv("DEVELOP_MODE", 0))
 # None means no timeout
 if (UNLOAD_MODEL_AFTER_S := os.getenv("UNLOAD_MODEL_AFTER_S", None)) is not None:
     UNLOAD_MODEL_AFTER_S = int(UNLOAD_MODEL_AFTER_S)
