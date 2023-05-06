12552c80125cc3890cf58a02f311b0d09eff59a6
nonchris <git@chris-ge.de>
2023-05-06 14:06:10 +0200

    move own imports down

--- src/whisper_api/__init__.py
+++ src/whisper_api/__init__.py
@@ -9,20 +9,18 @@ from typing import Callable, Optional
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from whisper_api.api_endpoints.endpoints import EndPoints
-from whisper_api.data_models.temp_dict import TempDict
-from whisper_api.frontend.endpoints import Frontend
-from whisper_api.data_models.threadsafe_dict import ThreadSafeDict
-from whisper_api.data_models.data_types import named_temp_file_name_t, uuid_hex_t
-from whisper_api.data_models.task import Task
-
 if __package__ is None and not hasattr(sys, "frozen"):
     import os.path
 
     path = os.path.realpath(os.path.abspath(__file__))
     sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
 
-
+from whisper_api.api_endpoints.endpoints import EndPoints
+from whisper_api.data_models.temp_dict import TempDict
+from whisper_api.frontend.endpoints import Frontend
+from whisper_api.data_models.threadsafe_dict import ThreadSafeDict
+from whisper_api.data_models.data_types import named_temp_file_name_t, uuid_hex_t
+from whisper_api.data_models.task import Task
 from whisper_api.environment import API_PORT, API_LISTEN, UNLOAD_MODEL_AFTER_S, DELETE_RESULTS_AFTER_M, \
     RUN_RESULT_EXPIRY_CHECK_M, REFRESH_EXPIRATION_TIME_ON_USAGE
 from whisper_api.version import __version__
