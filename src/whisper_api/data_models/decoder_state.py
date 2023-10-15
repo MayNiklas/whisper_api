ad876d5daddfd9cdb1f71c8ea5172461f447a4c3
nonchris <git@chris-ge.de>
2023-10-15 19:16:16 +0200

    add DecoderState model

---
+++ src/whisper_api/data_models/decoder_state.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from whisper_api.data_models.data_types import model_sizes_str_t
+
+
+class DecoderState(BaseModel):
+    """ Represents a state update from the decoder """
+    gpu_mode: bool = None
+    max_model_to_use: str = None
+    last_loaded_model_size: model_sizes_str_t = None
+    is_model_loaded: bool = None
+    queue_len: int = None
