e6a85b0705c142bee61f57ac0b94f48536a0eb73
MayNiklas <info@niklas-steffen.de>
2023-11-02 18:29:49 +0100

    actually test the API

--- flake.nix
+++ flake.nix
@@ -396,6 +396,41 @@
                   url = "https://openaipublic.azureedge.net/main/whisper/models/ed3a0b6b1c0edf879ad9b11b1af5a0e6ab5db9205f891f668f8b0e6c6326e34e/base.pt";
                   hash = "sha256-7ToLaxwO34ea2bEbGvWg5qtduSBfiR9mj4sObGMm404=";
                 });
+                test_file = (pkgs.fetchurl {
+                  url = "https://upload.wikimedia.org/wikipedia/commons/6/6d/En-Open_Source_Software_CD-article.ogg";
+                  hash = "sha256-rmgqP2cXZO0e/GAvLUYAaOa44kIiyooxwGQI/FbJuW4=";
+                });
+                python_for_test = pkgs.python3.withPackages (p: with p;  [ requests ]);
+                test_python = (pkgs.writeTextFile {
+                  name = "test.py";
+                  text = ''
+                    import time
+                    import requests
+                    import logging
+
+                    logger = logging.getLogger('logger')
+                    logger.setLevel(logging.DEBUG)
+
+                    url = 'http://192.168.0.1:3001/api/v1/'
+
+                    while (reply := requests.get(url + 'decoder_status').json()['is_model_loaded']) != True:
+                        logger.info("Waiting for model to load...")
+                        time.sleep(1)
+
+                    time.sleep(3)
+
+                    # post audio file)
+                    files = {'file': open('${test_file}', 'rb')}
+                    r = requests.post(url + 'transcribe', files=files).json()
+
+                    # get transcript
+                    while (reply := requests.get(url + 'status?task_id=' + r['task_id']).json())['status'] != 'finished':
+                        logger.info("Waiting for the transcript...")
+                        time.sleep(1)
+
+                    logger.info(reply['transcript'])
+                  '';
+                });
               in
               ''
                 start_all()
@@ -421,6 +456,9 @@
 
                 # check if client can reach API
                 client.wait_until_succeeds("${pkgs.curl}/bin/curl http://192.168.0.1:3001/api/v1/decoder_status")
+
+                # actually test the API
+                client.succeed("${python_for_test}/bin/python3 ${test_python}")
               '';
           };
       };
