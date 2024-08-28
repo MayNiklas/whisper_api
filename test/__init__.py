1cfdcff5ea26c4a57a69110e2ad0e13f1f2a1dd4
MayNiklas <info@niklas-steffen.de>
2024-08-28 11:27:14 +0200

    fix tests without module

---
+++ test/__init__.py
@@ -0,0 +1,9 @@
+"""
+This file is used to add the src directory to the python path so that the tests can import the modules from the src directory.
+"""
+
+import sys
+import os
+
+if "src" not in sys.path and "whisper_api" not in sys.modules:
+    sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(os.path.abspath(__file__)))) + "/src")
