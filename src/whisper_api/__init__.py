2d77714b7046ad353f6c8273722a7d33aeaeb774
MayNiklas <info@niklas-steffen.de>
2024-11-02 18:04:26 +0100

    update version to 20241101

--- src/whisper_api/__init__.py
+++ src/whisper_api/__init__.py
@@ -6,7 +6,7 @@ if __package__ is None and not hasattr(sys, "frozen"):
     path = os.path.realpath(os.path.abspath(__file__))
     sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
 
-__version__ = "20240825"
+__version__ = "20241101"
 
 from whisper_api.main import app  # isort: skip
 from whisper_api.main import start
