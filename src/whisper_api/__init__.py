55ae60eddd3362bb70db1db6b49cc86b52193fc3
nonchris <git@chris-ge.de>
2025-12-14 22:49:23 +0100

    ensure imports are _really_ only attempted on main

--- src/whisper_api/__init__.py
+++ src/whisper_api/__init__.py
@@ -6,10 +6,13 @@ if __package__ is None and not hasattr(sys, "frozen"):
     path = os.path.realpath(os.path.abspath(__file__))
     sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
 
-__version__ = "20241105"
+__version__ = "20251214"
 
-from whisper_api.main import app  # isort: skip
-from whisper_api.main import start
+
+from whisper_api.main import am_i_main_process
+if am_i_main_process():
+    from whisper_api.main import app  # isort: skip
+    from whisper_api.main import start
 
 if __name__ == "__main__":
     start()
